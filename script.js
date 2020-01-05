1f0967068a26a90c17a2a14db0c0dcb1538fa440
Artridge <48982618+ArtridgeGames@users.noreply.github.com>
2020-01-05 15:05:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,13 +20,28 @@ var objects = [
 var leftBtn = document.getElementById("left");
 var rightBtn = document.getElementById("right");
 var object = document.getElementById("object");
-
+var obj = undefined;
 function askObject() {
-  var obj = objects[Math.floor(Math.random() * objects.length)]
+  obj = objects[Math.floor(Math.random() * objects.length)]
   object.innerHTML = obj.name;
   object.style.top = "40%";
 }
 
 window.onload = function() {
   askObject();
+}
+
+leftBtn.onclick = function() {
+  click(true);
+}
+rightBtn.onclick = function() {
+  click(false);
+}
+
+function click(chosenOption) {
+  if (chosenOption == obj.recyclable) {
+    // correct
+  } else {
+    // incorrect
+  }
 }
