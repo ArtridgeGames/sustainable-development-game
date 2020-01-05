5d735e7ea6cf59f4c3739a0dcaaf82ea7f688810
Artridge <48982618+ArtridgeGames@users.noreply.github.com>
2020-01-05 14:48:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+var objects = [
+  {
+    name: "Objet 1",
+    recyclable: false
+  },
+  {
+    name: "Objet 2",
+    recyclable: true
+  },
+  {
+    name: "Objet 3",
+    recyclable: true
+  },
+  {
+    name: "Objet 4",
+    recyclable: false
+  }
+]
